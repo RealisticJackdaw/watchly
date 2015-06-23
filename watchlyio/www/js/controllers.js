a80a384fb65bd5fc1a80bec042a78b205b388e5f
Zachary Lester <zachary.greg.lester@gmail.com>
2015-06-23 16:31:45 -0700

    Create basic demo interface for main app in ionic

---
+++ watchlyio/www/js/controllers.js
@@ -0,0 +1,81 @@
+angular.module('starter.controllers', [])
+  // Stub service for dev markers
+  // Access with stubServ.stubs. ...
+  .service('stubServ', function() {
+    this.stubs = {
+
+        testDealer: {
+          pos: {lat: 37.783568, lng: -122.408840},
+          img: "./www/img/drug.png",
+          title: "STUB_DRUG"
+        },
+
+        testDealer2: {
+          pos: {lat: 37.783806, lng:  -122.408490},
+          img: "./www/img/drug.png",
+          title: "STUB_DRUG2"
+        },
+
+        testHazard: {
+          pos: {lat: 37.783844, lng: -122.409239},
+          img: "./www/img/hazard.png",
+          title: "STUB_ROAD_HAZARD"
+        },
+
+        testHazard2: {
+          pos: {lat: 37.783225, lng: -122.409102},
+          img: "./www/img/hazard.png",
+          title: "STUB_ROAD_HAZARD2"
+        },
+
+        testGraffiti: {
+          pos: {lat: 37.783901, lng: -122.409126},
+          img: "./www/img/graffiti.png",
+          title: "STUB_GRAFFITI"
+        }
+      };
+
+  })
+
+.controller('MapCtrl', function($scope, $ionicLoading, stubServ) {
+  
+  $scope.getStubs = function() {
+    return stubServ.stubs;
+  };
+
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.testAlertProfile = function() {
+    console.log("Clicked profile placeholder");
+    alert("You clicked profile placeholder");
+  };
+
+  $scope.testAlertForum = function() {
+    console.log("Clicked Forum placeholder");
+    alert("You clicked Forum placeholder");
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+});
+
+// .controller('NavCtrl', function($scope, $ionicLoading)
