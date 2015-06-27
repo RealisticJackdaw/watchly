72dd9c5ab1f47e55e43d5c033e441fe67e25256a
John Mai <maicorp@gmail.com>
2015-06-27 16:16:06 -0700

    (feature) setup the users function

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,4 +1,5 @@
-Users = require('../db/models/user');
+var util = require('../config/utility');
+var User = require('../db/models/user');
 
 
 module.exports = {
@@ -6,12 +7,45 @@ module.exports = {
     var username = req.body.username,
         password = req.body.password;
 
-
+    new User({username: username}).fetch().then(function(user){
+      if( !user ){  
+        res.status(401).send("Unknown user");
+      }
+      else {
+        user.comparePassword(password, function(match){
+          if( match) {
+            util.createSession(req, res, user);
+          } 
+          else {
+            res.redirect('/login');
+          }
+        });
+      }
+   });
   },
 
   signup: function (req, res, next) {
     var username  = req.body.username,
         password  = req.body.password;
+
+    new User({ username: username })
+    .fetch()
+    .then(function(user) {
+      if (!user) {
+        var newUser = new User({
+          username: username,
+          password: password
+        });
+        newUser.save().then(function(savedUser){
+          util.createSession(req, res, savedUser);
+        });
+      } 
+      else {
+        console.log('Account already exists');
+        res.redirect('/signup');
+      }
+    });    
+
   },
 
   checkAuth: function (req, res, next) {
@@ -19,5 +53,11 @@ module.exports = {
     // grab the token in the header is any
     // then decode the token, which we end up being the user object
     // check to see if that user exists in the database
+  },
+
+  signout: function (req, res, next) {
+    req.session.destroy(function(){
+      res.redirect('/login');
+    });
   }
 };
