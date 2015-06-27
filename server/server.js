02ef3e145f359e103059bdd063f3be42693d10be
BrianLoughnane <BrianJLoughnane@gmail.com>
2015-06-27 16:12:52 -0700

    (feature) add all wiring for incident db queries and responses to client

--- server/server.js
+++ server/server.js
@@ -2,19 +2,6 @@
 var express = require('express');
 var app = express();
 
-///////// testing ///////////
-var knex = require('./config/knex-config');
-var IncidentTypes = require('./db/collections/incidentTypes');
-var IncidentType = require('./db/models/incidentType');
-
-app.get('/getIncidentTypes', function (req, res) {
-  knex.select('type', 'iconFilename').from('incidentTypes')
-    .then(function (rows) {
-      res.send(rows);
-    });
-});
-////////////////////////////////
-
 require('./config/middleware.js')(app, express);
 
 // export our app for testing and flexibility, required by index.js
