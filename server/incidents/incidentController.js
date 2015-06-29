e6bfdce3af598ec0ea6d4a61fd681610c52a66bd
John Mai <maicorp@gmail.com>
2015-06-28 17:31:23 -0700

    (feature) add in incidenttype name

--- server/incidents/incidentController.js
+++ server/incidents/incidentController.js
@@ -22,7 +22,7 @@ module.exports = {
 
   allIncidents: function (req, res, next) {
     console.log('all incidents controller helper fired');
-    var query = 'select users.username, incidents.*, incidentTypes.iconFilename from incidents, users,incidentTypes where incidents.userid = users.id and incidents.incidentTypeId = incidentTypes.id';
+    var query = 'select users.username, incidents.*, incidentTypes.type, incidentTypes.iconFilename from incidents, users,incidentTypes where incidents.userid = users.id and incidents.incidentTypeId = incidentTypes.id';
     knex.raw(query)
     // knex.select('*').from('incident')
       .then(function (rows) {
